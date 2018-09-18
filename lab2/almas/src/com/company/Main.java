92ded9b9b053d866064e7f85fcbb16420b765cc3
Almajesty15 <Almaz.Oral2000@gmail.com>
2018-09-18 14:02:51 +0600

    f

--- lab2/almas/src/com/company/Main.java
+++ lab2/almas/src/com/company/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Student mystudent = new Student("Adilkhan", 123);
+        Student mystudent = new Student("Almas", 123);
 
     }
 }
